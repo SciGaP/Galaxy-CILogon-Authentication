c0c4de11b62d9a8a9c2d28bfd8e866c93cab8a04
marie-iub <yumai@iu.edu>
2013-09-23 17:51:28 +0000

    clean up
    git-svn-id: svn+ssh://svn.code.sf.net/p/ogce/svn/Galaxy@7897 9ee171a9-3b23-0410-aee4-96278d3b41b4

--- login_galaxy.php
+++
@@ -1,5 +0,0 @@
-<?php
-  session_start();
-  setrawcookie("GalaxyUser", $_SESSION['GalaxyUser']);
-  virtual('/galaxy/');
-?>
--- register_galaxy.php
+++
@@ -1,7 +0,0 @@
-<?php
-  session_start();
-  //setrawcookie("GalaxyUser", $_SESSION['GalaxyUser'], time()-10);
-  session_unset();
-  session_destroy();
-  header('Location: http://ncgas.org/questions.php');
-?>
